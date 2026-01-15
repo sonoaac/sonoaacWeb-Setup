ebb18b4f99139b23c6e82cd6acf30ca68d8229b1
mark1393 <48525266-mark1393@users.noreply.replit.com>
2026-01-15 03:38:24 +0000

    Initial commit

---
+++ attached_assets/main_1768448214511.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Main CSS for header, layout, mockup, footer
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
