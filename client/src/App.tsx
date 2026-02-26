81702615aec38f9bcfe0801e53b2f37bb86adad2
mark-sonoaac <mark@sonoaac.com>
2026-02-25 22:11:27 -0500

    Fix: Replace require() with ESM imports in App.tsx (Vite compatibility)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,20 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import MyTech from "@/pages/MyTech";
 import BuildPC from "@/pages/BuildPC";
 import Services from "@/pages/Services";
 import Contact from "@/pages/Contact";
+import BookConsultation from "@/pages/BookConsultation";
+import ITSupport from "@/pages/ITSupport";
+import OnSiteServices from "@/pages/OnSiteServices";
+import DeviceSetup from "@/pages/DeviceSetup";
+import BuyReadyComputer from "@/pages/BuyReadyComputer";
+import SoftwareFixes from "@/pages/SoftwareFixes";
+import BusinessIT from "@/pages/BusinessIT";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 
@@ -16,13 +24,13 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/book-consultation" component={require('./pages/BookConsultation').default} />
-      <Route path="/it-support" component={require('./pages/ITSupport').default} />
-      <Route path="/on-site-services" component={require('./pages/OnSiteServices').default} />
-      <Route path="/device-setup" component={require('./pages/DeviceSetup').default} />
-      <Route path="/buy-ready-computer" component={require('./pages/BuyReadyComputer').default} />
-      <Route path="/software-fixes" component={require('./pages/SoftwareFixes').default} />
-      <Route path="/business-it" component={require('./pages/BusinessIT').default} />
+      <Route path="/book-consultation" component={BookConsultation} />
+      <Route path="/it-support" component={ITSupport} />
+      <Route path="/on-site-services" component={OnSiteServices} />
+      <Route path="/device-setup" component={DeviceSetup} />
+      <Route path="/buy-ready-computer" component={BuyReadyComputer} />
+      <Route path="/software-fixes" component={SoftwareFixes} />
+      <Route path="/business-it" component={BusinessIT} />
       <Route path="/my-tech" component={MyTech} />
       <Route path="/my-tech/build-pc" component={BuildPC} />
       <Route path="/build-pc" component={BuildPC} />
